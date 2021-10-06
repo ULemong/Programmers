d172d5e161a4bed17d47dd987d32f5f25dd64c1d
ULemong <86288617+ULemong@users.noreply.github.com>
2021-10-06 20:58:56 +0900

    문자열을 정수로 바꾸기

---
+++ Level 1/연습 문제/문자열을 정수로 바꾸기/solution.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    var answer = 0;
+    answer = Number(s);
+    return answer;
+}
+
+/* another solution */
+function solution(s) {
+    return s / 1; // s가 자동으로 정수로 변환됨
+}
