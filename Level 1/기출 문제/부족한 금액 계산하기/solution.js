18556eaeedb3586d1475a21b5389f56a9c04a960
ULemong <86288617+ULemong@users.noreply.github.com>
2021-10-07 02:42:18 +0900

    부족한 금액 계산하기

--- Level 1/기출 문제/부족한 금액 계산하기/solution.js
+++ Level 1/기출 문제/부족한 금액 계산하기/solution.js
@@ -10,9 +10,7 @@ function solution(price, money, count) {
 
 /* another solution */
 function solution(price, money, count) {
-    let sum = 0;
+    let sum = price * (((1 + count) * count) / 2) - money; // 가우스 공식
     
-    sum = price * (((1 + count) * count) / 2); // 가우스 공식
-    
-    return (sum > money) ? sum - money : 0;
+    return (sum > 0) ? sum : 0;
 }
