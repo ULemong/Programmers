e95559c7cc0aef5ac0ea42fe36025005b3ff6db1
ULemong <86288617+ULemong@users.noreply.github.com>
2021-10-21 23:59:31 +0900

    완주하지 못한 선수

--- Level 1/연습 문제/완주하지 못한 선수/solution.js
+++ Level 1/연습 문제/완주하지 못한 선수/solution.js
@@ -1,4 +1,23 @@
 function solution(participant, completion) {
+    let answer = '';
+    let i = 0;
+    
+    participant.sort();
+    completion.sort();
+    
+    while(true) {
+        if (participant[i] !== completion[i]) {
+            answer += participant[i];
+            break;
+        }
+        i++;
+    }
+    return answer;
+}
+
+
+/* another solution */
+function solution(participant, completion) {
     participant.sort();
     completion.sort();
     
