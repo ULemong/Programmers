ba797366a317925c004379ebc77b23c6defa4b2f
ULemong <86288617+ULemong@users.noreply.github.com>
2021-10-25 22:25:09 +0900

    핸드폰 번호 가리기

---
+++ Level 1/연습 문제/핸드폰 번호 가리기/solution.js
@@ -0,0 +1,8 @@
+function solution(phone_number) {
+    let answer = '';
+    
+    answer += '*'.repeat(phone_number.length - 4) 
+        + phone_number.slice(phone_number.length - 4);
+        
+    return answer;
+}
