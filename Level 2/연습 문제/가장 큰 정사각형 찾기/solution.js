b80f9528fda28bbce54772fddc06e16170dd3e3a
ULemong <86288617+ULemong@users.noreply.github.com>
2021-11-04 22:11:55 +0900

    가장 큰 정사각형 찾기

---
+++ Level 2/연습 문제/가장 큰 정사각형 찾기/solution.js
@@ -0,0 +1,19 @@
+function solution(board)
+{
+    let answer = 0;
+    
+    // board.unshift(new Array(board[0].length).fill(0));
+    // board.map(arr => arr.unshift(0));
+    if (board.length <= 1 || board[0].length <= 1) return 1;
+    
+    for (let i = 1; i < board.length; i++) {
+        for (let j = 1; j < board[0].length; j++) {
+            if (board[i][j]) {
+                board[i][j] = Math.min(board[i-1][j-1], board[i-1][j], board[i][j-1]) + 1;
+                answer = Math.max(answer, board[i][j]);
+            }
+        }
+    }
+    
+    return answer ** 2;
+}
