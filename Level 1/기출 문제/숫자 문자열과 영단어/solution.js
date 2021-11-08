6f2a68f75e8f50e5e251f3128ca2a676467d6471
ULemong <86288617+ULemong@users.noreply.github.com>
2021-11-08 20:05:55 +0900

    숫자 문자열과 영단어

---
+++ Level 1/기출 문제/숫자 문자열과 영단어/solution.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    const NUMBER = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+    for(let i = 0; i < NUMBER.length; i++) {
+        if (!s.includes(NUMBER[i])) continue;
+        
+        let arr = s.split(NUMBER[i]);
+        s = arr.join(i);
+    }
+    
+    return +(s);
+}
