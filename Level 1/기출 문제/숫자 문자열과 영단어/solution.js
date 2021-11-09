f4ee839a8f2967e8cdea5085cd97ebe22b6b46c4
ULemong <86288617+ULemong@users.noreply.github.com>
2021-11-09 23:27:57 +0900

    숫자 문자열과 영단어

--- Level 1/기출 문제/숫자 문자열과 영단어/solution.js
+++ Level 1/기출 문제/숫자 문자열과 영단어/solution.js
@@ -2,7 +2,7 @@ function solution(s) {
     const NUMBER = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
     for(let i = 0; i < NUMBER.length; i++) {
-        if (!s.includes(NUMBER[i])) continue
+        if (!s.includes(NUMBER[i])) continue;
         
         let arr = s.split(NUMBER[i]);
         s = arr.join(i);
