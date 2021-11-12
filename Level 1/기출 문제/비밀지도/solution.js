07e622004e8181623270d260942c3683787728f4
ULemong <86288617+ULemong@users.noreply.github.com>
2021-11-13 02:31:54 +0900

    비밀지도

--- Level 1/기출 문제/비밀지도/solution.js
+++ Level 1/기출 문제/비밀지도/solution.js
@@ -1,21 +1,20 @@
 function solution(n, arr1, arr2) {
-    let answer = [];
- 
-    for (let i = 0; i < n; i++) {
-        let result = '';
-        
-        let arr1_binary = arr1[i].toString(2);
-        let arr2_binary = arr2[i].toString(2);
-        
-        arr1_binary = '0'.repeat(n - arr1_binary.length) + arr1_binary;
-        arr2_binary = '0'.repeat(n - arr2_binary.length) + arr2_binary;
+  let answer = [];
 
-        for (let j = 0; j < n; j++) {
-            if (arr1_binary[j] === '1') result += '#';
-            else result += (arr2_binary[j] === '1') ? '#' : ' ';
-        }
-        answer.push(result);
-    }
-    
-    return answer;
+  
+  for (let i = 0; i < n; i++) {
+      let result = '';
+      
+      let arr1_binary = arr1[i].toString(2);
+      let arr2_binary = arr2[i].toString(2);
+      
+      arr1_binary = '0'.repeat(n - arr1_binary.length) + arr1_binary;
+      arr2_binary = '0'.repeat(n - arr2_binary.length) + arr2_binary;
+
+      for (let j = 0; j < n; j++) 
+          result += (+(arr1_binary[j]) || +(arr2_binary[j])) ? '#' : ' ';
+      
+      answer.push(result);
+  }
+  return answer;
 }
