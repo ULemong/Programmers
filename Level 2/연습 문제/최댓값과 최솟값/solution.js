50323ba2c2e5eba4f2cee00f9cc3fea52d054b0e
ULemong <86288617+ULemong@users.noreply.github.com>
2021-11-15 22:38:22 +0900

    최댓값과 최솟값

---
+++ Level 2/연습 문제/최댓값과 최솟값/solution.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    let arr = s.split(' ');
+    arr.sort((x, y) => x - y);
+
+    return (arr[0] + ' ' + arr[arr.length - 1]);
+}
+
+/* another solution */
+function solution(s) {
+    const arr = s.split(' ');
+    
+    // string 자료형도 Math.min, Math.min 메서드 사용 가능
+    return (Math.min(...arr) + ' ' + Math.max(...arr));
+}
