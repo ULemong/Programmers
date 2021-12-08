d759f1abd1c7a034d96ffe4f193fd1164d8136a2
ULemong <86288617+ULemong@users.noreply.github.com>
2021-12-08 21:45:46 +0900

    올바른 괄호

---
+++ Level 2/연습 문제/올바른 괄호/solution.js
@@ -0,0 +1,12 @@
+function solution(s){
+    let stack = [];
+    
+    for (let i of s) {
+        if (i === '(') stack.push(i);
+        else if (i === ')') {
+            if (!stack.length) return false;
+            stack.pop();
+        }
+    }
+    return stack.length ? false : true;
+}
