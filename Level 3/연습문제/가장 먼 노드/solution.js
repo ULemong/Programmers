a7907ee12960bffecf434af1fa9ac4500831897b
ULemong <86288617+ULemong@users.noreply.github.com>
2022-01-24 04:51:40 +0900

    가장 먼 노드

--- Level 3/연습문제/가장 먼 노드/solution.js
+++ Level 3/연습문제/가장 먼 노드/solution.js
@@ -1,29 +1,31 @@
 function solution(n, edge) {
-    return bfs(1, edge, n);
-}
-
-const bfs = (start, arr, end) => {
-    const visited = new Array(end + 1).fill(false);
-    const level = new Array(end + 1).fill(0);
-    const queue = [start];
-    visited[start] = true;
+    const visited = Array.from(Array(n + 1).fill(0));
+    const level = Array.from(Array(n + 1).fill(0));
     
-    while (queue.length) {
-        const head = queue.shift();
-        const lev = level[head] + 1;
-        
-        for (let node of arr) {
-            if (node[0] === head && !visited[node[1]]) {
-                queue.push(node[1]);
-                visited[node[1]] = true;
-                level[node[1]] = lev;
-            } else if (node[1] === head && !visited[node[0]]) { 
-                queue.push(node[0]);
-                visited[node[0]] = true;
-                level[node[0]] = lev;
+    const bfs = (start, arr) => {
+        const queue = [start];
+        visited[start] = 1;
+
+        while (queue.length) { 
+            const node = queue.shift();
+            const currentLevel = level[node] + 1; 
+
+            for (let vertex of edge) {
+                if (vertex[0] === node && !visited[vertex[1]]) {
+                    queue.push(vertex[1]);
+                    visited[vertex[1]] = 1;
+                    level[vertex[1]] = currentLevel;
+                } else if (vertex[1] === node && !visited[vertex[0]]) { 
+                    queue.push(vertex[0]);
+                    visited[vertex[0]] = 1;
+                    level[vertex[0]] = currentLevel;
+                }
             }
-        }
-    } 
-    const maxLevel = Math.max.apply(null, level);
-    return level.filter((i) => i === maxLevel).length;
+        } 
+    };
+    
+    bfs(1, edge);
+    
+    const max = Math.max(...level);
+    return level.filter((i) => i === max).length;
 }
