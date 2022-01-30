698c6dbb8381229736100f5612431d2437a7b157
ULemong <86288617+ULemong@users.noreply.github.com>
2022-01-30 19:23:42 +0900

    solution

--- Level 3/연습문제/가장 먼 노드/solution.js
+++ Level 3/연습문제/가장 먼 노드/solution.js
@@ -29,3 +29,33 @@ function solution(n, edge) {
     const max = Math.max(...level);
     return level.filter((i) => i === max).length;
 }
+
+/* another solution */
+function solution(n, vertex) {
+  const graph = Array.from(Array(n + 1), () => []);
+  vertex.forEach(([v1, v2]) => {
+    graph[v1].push(v2);
+    graph[v2].push(v1);
+  });
+
+  const visited = Array(n + 1).fill(false);
+  const queue = [];
+
+  queue.push([1, 0]);
+  visited[1] = 0;
+
+  while (queue.length) {
+    let [to, cost] = queue.shift();
+
+    graph[to].map(nextNode => {
+      const nextCost = cost + 1;
+      if (visited[nextNode] === false) {
+        visited[nextNode] = nextCost;
+        queue.push([nextNode, nextCost]);
+      }
+    });
+  }
+
+  const max = Math.max(...visited);
+  return visited.filter(v => v === max).length;
+}
