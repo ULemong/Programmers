0f8f46ad68b063096ec2a7dad8d922ea053846c8
ULemong <86288617+ULemong@users.noreply.github.com>
2022-01-30 19:24:21 +0900

    solution

--- Level 3/연습문제/순위/solution.js
+++ Level 3/연습문제/순위/solution.js
@@ -34,3 +34,39 @@ function solution(n, results) {
 
   return answer;
 }
+
+/* another solution */
+function solution(n, results) {
+  const graph = Array.from(Array(n + 1), () => Array(n + 1).fill(false));
+  results.forEach(([win, lose]) => {
+    graph[win][lose] = 1;
+    graph[lose][win] = -1;
+    graph[win][win] = 0;
+    graph[lose][lose] = 0;
+  });
+
+  for (let k = 1; k <= n; k++) {
+    for (let i = 1; i <= n; i++) {
+      for (let j = 1; j <= n; j++) {
+        if (graph[i][k] === 1 && graph[k][j] === 1) {
+          graph[i][j] = 1;
+        }
+        if (graph[i][k] === -1 && graph[k][j] === -1) {
+          graph[i][j] = -1;
+        }
+      }
+    }
+  }
+  let answer = 0;
+  for (let i = 1; i <= n; i++) {
+    let flag = true;
+    for (let j = 1; j <= n; j++) {
+      if (graph[i][j] === false) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) answer++;
+  }
+  return answer;
+}
