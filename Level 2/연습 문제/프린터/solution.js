76a738465d40fd7c5a6e5ee2135e48a86dc212f1
ULemong <86288617+ULemong@users.noreply.github.com>
2022-02-08 23:49:55 +0900

    프린터

--- Level 2/연습 문제/프린터/solution.js
+++ Level 2/연습 문제/프린터/solution.js
@@ -1,4 +1,17 @@
 function solution(priorities, location) {
-    var answer = 0;
-    return answer
+    let answer = 0;
+    const pri = priorities.map((v, i) => [v, i]);
+    const arr = [];
+
+    while (pri.length !== 0) {
+        let now = pri.shift();
+        if (pri.some((a) => now[0] < a[0])) pri.push(now);
+        else {
+            arr.push(now);
+            if (arr[answer][1] === location) break;
+            else answer++;
+        }
+    }
+
+    return answer + 1;
 }
