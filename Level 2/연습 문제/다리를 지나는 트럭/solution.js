97a86ff2a3e6ee268cc9d43d345e9e1f4822fcd7
ULemong <86288617+ULemong@users.noreply.github.com>
2022-02-12 23:42:23 +0900

    스택/큐

---
+++ Level 2/연습 문제/다리를 지나는 트럭/solution.js
@@ -0,0 +1,31 @@
+function solution(bridge_length, weight, truck_weights) {
+  if (truck_weights.length === 1) return bridge_length + 1;
+
+  let bridge = Array.from(Array(bridge_length).fill(0));
+  bridge[bridge_length - 1] = truck_weights.shift();
+  let answer = 1;
+
+  while (true) {
+    let bridgeWeight = bridge.reduce((prev, curr) => prev + curr);
+
+    if (bridgeWeight - bridge[0] + truck_weights[0] <= weight) {
+      let truck = truck_weights.shift();
+      bridge.shift();
+      bridge.push(truck);
+
+      bridgeWeight += truck;
+      answer += 1;
+    } else {
+      bridge.shift();
+      bridge.push(0);
+      answer += 1;
+    }
+
+    if (!truck_weights.length) {
+      answer += bridge_length;
+      break;
+    }
+  }
+
+  return answer;
+}
