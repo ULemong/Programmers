5a9559546d758afc96e24f2d186e2cf4419ed50e
ULemong <86288617+ULemong@users.noreply.github.com>
2022-02-16 20:07:30 +0900

    정렬

--- Level 1/연습 문제/K번째수/solution.js
+++ Level 1/연습 문제/K번째수/solution.js
@@ -1,15 +1,12 @@
 function solution(array, commands) {
     let answer = [];
-
+    
     for (let i of commands) {
-        let arr = [];
-
-        arr = array.slice(i[0] - 1, i[1])
-        arr.sort((x, y) => x - y);
-
-        answer.push(arr[i[2] - 1]);
+        let newArray = array.slice(i[0] - 1, i[1]);
+        newArray.sort((x, y) => x - y);
+        answer.push(newArray[i[2] - 1]);
     }
-
+    
     return answer;
 }
 
