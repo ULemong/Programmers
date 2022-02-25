f9d32a1a87285b3d8999f94ec5e4f56ee9f19de1
ULemong <86288617+ULemong@users.noreply.github.com>
2022-02-25 20:44:06 +0900

    두 정수 사이의 합

--- Level 1/연습 문제/두 정수 사이의 합/solution.js
+++ Level 1/연습 문제/두 정수 사이의 합/solution.js
@@ -13,3 +13,27 @@ function solution(a, b) {
 function solution(a, b) {
     return (a + b) * (Math.abs(a - b) + 1) / 2; // 가우스 공식
 }
+
+/* another solution */
+function solution(a, b) {
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+    let sum = 0;
+
+    if ((a + b) % 2 === 0) {
+        for (let i = a; i < (a + b) / 2; i++) {
+            sum += a + b;
+        }
+        return sum + (a + b) / 2;
+    }
+    else {
+        for (let i = a; i < Math.ceil((a + b) / 2); i++) {
+            sum += a + b;
+        }
+        return sum;
+    }
+}
