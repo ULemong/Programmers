3cd8d7e11e7e1027e5f058af2f328bc44da6ee51
ULemong <86288617+ULemong@users.noreply.github.com>
2022-04-12 01:27:50 +0900

    조합 & Hash

--- Level 2/기출 문제/메뉴 리뉴얼/solution.js
+++ Level 2/기출 문제/메뉴 리뉴얼/solution.js
@@ -1,4 +1,43 @@
-function solution(orders, course) {
+function getCombination(arr, n, bucket) {
+  let result = [];
+  
+    if (n === 0) {
+        return bucket.join('');
+    }
+
+  for (let i = 0; i < arr.length; i++) {
+    let rest = arr.slice(i + 1);
+    let pick = arr[i];
+
+    let combination = getCombination(rest, n - 1, bucket.concat(pick));
+    result.push(combination);
+  }
+    
+  return result;
+}
+
+function solution(orders, course) {  
     let answer = [];
-    return answer;
+    
+    for (let num of course) {
+        let dic = {};
+        
+        for (let i of orders) {
+            let sort = i.split('').sort();
+            let combi = getCombination(sort, num, []).flat(num - 1);
+            
+            for (let menu of combi) {
+                dic[menu] ? dic[menu]++ : dic[menu] = 1;
+            }
+        }
+        
+        let max = Math.max(...Object.values(dic));
+        if (max === 1) continue;
+        
+        for (let i in dic) {
+            if (dic[i] === max) answer.push(i);
+        }
+    }
+    
+    return answer.sort();
 }
