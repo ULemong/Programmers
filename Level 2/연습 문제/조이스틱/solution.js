781eb3090f2f3f3afd813eea3329c27596c547aa
ULemong <86288617+ULemong@users.noreply.github.com>
2022-04-20 23:06:47 +0900

    탐욕법(Greedy)

--- Level 2/연습 문제/조이스틱/solution.js
+++ Level 2/연습 문제/조이스틱/solution.js
@@ -4,7 +4,7 @@ function solution(name) {
     let move = length - 1; // 좌우 조작 (순서대로 이동하는 경우)
     let index;
     
-    // "BBBBAAAAAAAB", 10
+    // "BBBBAAAAAAAD", 10
     for(let i = 0; i < length; i++) {
         let ascii = name[i].charCodeAt(0);
         if (ascii < 79) answer += ascii - 65; // A ~ N  
@@ -15,8 +15,10 @@ function solution(name) {
         // 연속하는 A의 개수
         while(index < length && name[index] === 'A') index++;
         
-        move = Math.min(move, i * 2 + length - index); // min(순서대로 가는 경우, 순서대로 가다가 되돌아가는 경우)      
+        move = Math.min(move, i * 2 + length - index); // min(순서대로 가는 경우, 순서대로 가다가 되돌아가는 경우)  
+                                                       // B->B->B->B->A->A->A->A->A->A->A->B (11), (B->B->B->B B<-B<-B<-B)->D (6+1 = 7) 
         move = Math.min(move, (length - index) * 2 + i); // min(move, 뒤에부터 먼저 가는 경우)
+                                                         // (7), (B->D B<-D)->B->B->B (2+3 = 5)
     }
     
     return answer + move;
