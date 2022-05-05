891c16b0a71d10e1920b6c96353311805898e396
ULemong <86288617+ULemong@users.noreply.github.com>
2022-05-05 22:16:13 +0900

    이분탐색

--- Level 2/기출 문제/예상 대진표/solution.js
+++ Level 2/기출 문제/예상 대진표/solution.js
@@ -1,30 +1,35 @@
-function solution(n,a,b) {
-    var answer = 0;
+function solution(n, a, b) { 
+    let answer = 0;
+    let mid = parseInt(n / 2);
     if (a > b) {
         let temp = a;
         a = b;
         b = temp;
     }
-    
-    let mid = parseInt(n / 2);
-    
-    while (true) {
-      if (b - a === 1) return 1;
-    
-        if (a <= mid && b <= mid) {
-            mid = parseInt(mid / 2); // 4
 
+    if (Math.ceil(a / 2) === Math.ceil(b / 2)) return 1;
+
+    while (true) {
+        if (b - a === 1) {
+            if (a % 2 !== 0) return answer + 1;
+            else if (mid === 1) return answer;
+        }
+
+        if (a <= mid && b <= mid) {
+            mid = parseInt(mid / 2);
         }
         else if (a <= mid && b > mid)  {
-            answer++
-            a = Math.ceil(a / 2) // 2
-            b = Math.ceil(a / 2) // 6
-            mid = parseInt(mid / 2) // 2
+            answer++;
+            a = Math.ceil(a / 2);
+            b = Math.ceil(b / 2);
+            mid = parseInt(mid / 2);
+
         } 
         else if (a > mid && b > mid) { 
+            a -= mid; 
+            b -= mid; 
+            mid = parseInt(mid / 2);
+        }
 
-        }  
-    }
-         
-    return answer;
+    }   
 }
