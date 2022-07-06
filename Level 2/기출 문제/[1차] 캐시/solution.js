57c80db1546dc31feb8778b4eb01682795ad93d6
ULemong <86288617+ULemong@users.noreply.github.com>
2022-07-07 01:44:32 +0900

    Array

--- Level 2/기출 문제/[1차] 캐시/solution.js
+++ Level 2/기출 문제/[1차] 캐시/solution.js
@@ -1,23 +1,69 @@
 function solution(cacheSize, cities) {
-  if (cacheSize === 0) return cities.length * 5;
-  cities = cities.map(v => v.toUpperCase())
-  let queue = [];
-  let answer = 0;
-  let i = 0;
-  while (i < cities.length) {
-      const newData = cities[i++]
-      let searchIndex = queue.indexOf(newData);
+  const cache = [];
+  let time = 0;
 
-      if (searchIndex === -1) {
-        if(cacheSize === queue.length) queue.shift();
+  if (!cacheSize) return cities.length * 5;
 
-        answer += 5;
-      } else {
-        queue.splice(searchIndex, 1);
-        answer += 1;
+  for (let city of cities) {
+    city = city.toLowerCase();
+
+    // 1. city가 cache에 없는 경우
+    if (!cache.includes(city)) {
+      // 1-1) cache 공간 있는 경우
+      if (cache.length < cacheSize) {
+        cache.push(city);
       }
-      queue.push(newData);
+      // 1-2) cache 공간 없는 경우
+      else {
+        cache.shift();
+        cache.push(city);
+      }
+
+      time += 5;
     }
 
-  return answer;
+    // 2. city가 cache에 있는 경우
+    else {
+      const index = cache.indexOf(city);
+      cache.splice(index, 1);
+      cache.push(city);
+      time += 1;
+    }
+  }
+
+  return time;
+}
+
+/* another solution */
+function solution(cacheSize, cities) {
+  const cache = [];
+  let time = 0;
+  let index = 0;
+
+  if (!cacheSize) return cities.length * 5;
+
+  while (index < cities.length) {
+    const city = cities[index++].toLowerCase();
+    const searchIndex = cache.indexOf(city);
+
+    // 1. city가 cache에 없는 경우
+    if (searchIndex === -1) {
+      // cache 공간 없는 경우
+      if (cache.length >= cacheSize) {
+        cache.shift();
+      }
+        
+      time += 5;
+    }
+      
+    // 2. city가 cache에 있는 경우
+    else {
+      cache.splice(searchIndex, 1);
+      time += 1;
+    }
+
+    cache.push(city); // 모든 경우에 push 발생
+  }
+
+  return time;
 }
