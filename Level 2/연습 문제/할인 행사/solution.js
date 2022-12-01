8a855fcd47507ed7735962f6f2a1363f6c8deb35
ULemong <86288617+ULemong@users.noreply.github.com>
2022-12-01 23:39:46 +0900

    array

--- Level 2/연습 문제/할인 행사/solution.js
+++ Level 2/연습 문제/할인 행사/solution.js
@@ -26,3 +26,25 @@ function solution(want, number, discount) {
 
   return answer;
 }
+
+/* another solution */
+function solution(want, number, discount) {
+  var answer = 0;
+  const info = {};
+  for (let i = 0; i < number.length; i++) {
+    info[`${want[i]}`] = info[`${want[i]}`] || number[i];
+  }
+  for (let i = 0; i <= discount.length - 10; i++) {
+    let flag = true;
+    for (let w of want) {
+      if (
+        discount.slice(i, i + 10).filter((x) => x === w).length < info[`${w}`]
+      ) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) answer++;
+  }
+  return answer;
+}
